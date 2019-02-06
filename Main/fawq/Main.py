e8f22fbec2220d4fe730c9a2fd5edeaa7caee4ec
Krystian Krakowski <krystian.adr@wp.pl>
2019-02-06 23:04:11 +0100

    Add new method to new project

--- Main/fawq/Main.py
+++ Main/fawq/Main.py
@@ -2,5 +2,6 @@ import numpy as np
 from NeuralNetworks.RealNeuralNetwork.NeuralNetwork import NeuralNetwork
 
 if __name__ == "__main__":
-    start = NeuralNetwork([3, 3, 2, 2])
+    start = NeuralNetwork([3, 3, 2, 1])
     start.show_all()
+    print(start.get_value([1, 2, 3]))
--- NeuralNetworks/RealNeuralNetwork/NeuralNetwork.py
+++ NeuralNetworks/RealNeuralNetwork/NeuralNetwork.py
@@ -5,9 +5,10 @@ class NeuralNetwork:
     def __init__(self, array_of_lengths):
         self.all_biases = []
         self.all_weights = []
+        self.all_nodes = []
 
         for i in range(len(array_of_lengths)-1):
-            biases = np.random.rand(array_of_lengths[i]).tolist()
+            biases = np.random.rand(array_of_lengths[i+1]).tolist()
             self.all_biases.append(biases)
 
             weights = []
@@ -17,11 +18,28 @@ class NeuralNetwork:
 
             self.all_weights.append(weights)
 
+        for i in range(len(array_of_lengths)):
+            nodes = np.random.rand(array_of_lengths[i]).tolist()
+            self.all_nodes.append(nodes)
+
         self.all_biases = np.asarray(self.all_biases)
         self.all_weights = np.asarray(self.all_weights)
+        self.all_nodes = np.asarray(self.all_nodes)
 
     def show_all(self):
         print("All biases:")
         print(self.all_biases)
         print("All weights:")
         print(self.all_weights)
+        print("All random nodes:")
+        print(self.all_nodes)
+
+    # Need to check
+    def get_value(self, data):
+        self.all_nodes[0] = data
+
+        for i in range(len(self.all_nodes)-1):
+            self.all_nodes[i+1] = (np.matrix(self.all_nodes[i])*np.matrix(self.all_weights[i]).T +
+                                   np.matrix(self.all_biases[i]))
+
+        return self.all_nodes[len(self.all_nodes)-1]
