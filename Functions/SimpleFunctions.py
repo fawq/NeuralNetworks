890ffa5861d751366d2d5a789daee23f1c9ff2e6
Krystian Krakowski <krystian.adr@wp.pl>
2019-02-06 22:03:14 +0100

    Add new project and rename old

--- Functions/SimpleFunctions.py
+++ Functions/SimpleFunctions.py
@@ -2,4 +2,4 @@ import numpy as np
 
 
 def sigmoid(x, derivative=False):
-    return (1 / (1 + np.exp(-x))) if derivative == False else x*(1-x)
+    return (1 / (1 + np.exp(-x))) if derivative == False else sigmoid(x)*(1-sigmoid(x))
