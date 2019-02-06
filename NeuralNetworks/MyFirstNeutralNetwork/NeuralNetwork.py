b9ad4a0d1b499017cb005e0ea8d4a008e5c2cd9d
Krystian Krakowski <krystian.adr@wp.pl>
2019-02-06 10:31:02 +0100

    Add first stage of work with neural networks

---
+++ NeuralNetworks/MyFirstNeutralNetwork/NeuralNetwork.py
@@ -0,0 +1,16 @@
+import numpy as np
+from Functions.SimpleFunctions import sigmoid
+
+
+class NeuralNetwork:
+    def __init__(self, x, y):
+        self.input = x
+        self.weights1 = np.random.rand(self.input.shape[1], 4)
+        self.weights2 = np.random.rand(4, 1)
+        self.y = y
+        self.output = np.zeros(y.shape)
+
+    def feedforward(self):
+        self.layer1 = sigmoid(np.dot(self.input, self.weights1))
+        self.output = sigmoid(np.dot(self.layer1, self.weights2))
+
