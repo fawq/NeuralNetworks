8d7f4c8e39393bca5d4c6925377c2649c6cb29fc
Krystian Krakowski <krystian.adr@wp.pl>
2019-02-12 09:54:11 +0100

    Add new project and new methods

--- Functions/SimpleFunctions.py
+++ Functions/SimpleFunctions.py
@@ -3,3 +3,7 @@ import numpy as np
 
 def sigmoid(x, derivative=False):
     return (1 / (1 + np.exp(-x))) if derivative is False else sigmoid(x) * (1 - sigmoid(x))
+
+
+def relu(x, derivative=False):
+    return max(0, x) if derivative is False else 1 if x > 0 else 0
